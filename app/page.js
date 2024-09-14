045422a42b0cd95de462fd9737fccdbca42a81ef
Shadhadeepak <shadhadeepak20@gmail.com>
2024-09-15 00:41:56 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
     <div className="">
-      <h1 className="flex items-center justify-center">hello World!</h1>
-        <Button>Shadha deepak</Button>
+      <h1 className="flex items-center justify-center"></h1>
+        <Dashboard />
     </div>
   );
 }
